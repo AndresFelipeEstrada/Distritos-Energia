bca03a40b750c256d4c2405c60e9316beb504d37
Andres <andresfe1396@gmail.com>
2023-02-26 10:13:13 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Select from 'react-select'
 import { Tables } from './components/Tables'
 import { useForm } from './hooks/useForm'
 import { useState } from 'react'
-import { selectStyle } from './logic/selectStyles'
+import { customStyles } from './logic/selectStyles'
 
 function App () {
   const { caudal, setCaudal, temp1, setTemp1, temp2, setTemp2, servicio, setServicio, error } = useForm('')
-  const [select, setSelect] = useState('')
+
+  const [centrifugo, setCentrifugo] = useState('')
+  const [cantCentrifugo, setCantCentrifugo] = useState('')
+  const [absorcion, setAbsorcion] = useState('')
+  const [cantAbsorcion, setCantAbsorcion] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -17,12 +21,9 @@ function App () {
 
   const total = (Number(caudal) * (Number(temp1) - Number(temp2)) * Number(servicio) * 1000 * 0.0003069).toFixed(0)
 
-  // const redirectTables = () => {
-  //   window.location.replace('http://localhost:5173/tables')
-  // }
   const selectOptios = [
     { label: '500', value: 500 },
-    { label: '700', value: 700 },
+    { label: '750', value: 750 },
     { label: '1000', value: 1000 }
   ]
 
@@ -36,26 +37,42 @@ function App () {
 
           <div className='inputContainer'>
             <input className='input' type='number' placeholder='caudal' onChange={e => setCaudal(e.target.value)} value={caudal} />
-            <label className='label' htmlFor=''>Caudal:</label>
+            <label className='label'>Caudal:</label>
           </div>
 
           <div className='inputContainer'>
             <input className='input' type='number' placeholder='temperatura de entrada' onChange={e => setTemp1(e.target.value)} value={temp1} />
-            <label className='label' htmlFor=''>Temperatura de entrada:</label>
+            <label className='label'>Temperatura de entrada:</label>
           </div>
 
           <div className='inputContainer'>
             <input className='input' type='number' placeholder='temperatura de salida' onChange={e => setTemp2(e.target.value)} value={temp2} />
-            <label className='label' htmlFor=''>Temperatura de salida:</label>
+            <label className='label'>Temperatura de salida:</label>
           </div>
 
           <div className='inputContainer'>
             <input className='input' type='number' placeholder='factor servicio' onChange={e => setServicio(e.target.value)} value={servicio} />
-            <label className='label' htmlFor=''>Servicio:</label>
+            <label className='label'>Servicio:</label>
           </div>
 
-          <div className='inputContainer'>
-            <Select styles={selectStyle()} options={selectOptios} onChange={e => setSelect(e.value)} placeholder='chiller:' />
+          <div className='column'>
+            <div className='inputContainer'>
+              <Select styles={customStyles} options={selectOptios} onChange={e => setCentrifugo(e.value)} placeholder='chiller centrifugos:' />
+            </div>
+
+            <div className='inputContainer columnCant'>
+              <input className='input' type='text' placeholder='Cantidad' />
+              <label className='label'>Cantidad:</label>
+            </div>
+
+            <div className='inputContainer columnSelect'>
+              <Select styles={customStyles} options={selectOptios} onChange={e => setAbsorcion(e.value)} placeholder='chiller absorcion:' />
+            </div>
+
+            <div className='inputContainer columnCant'>
+              <input className='input' type='text' placeholder='Cantidad' />
+              <label className='label'>Cantidad:</label>
+            </div>
           </div>
 
           <button className='submitBtn'>Enviar</button>
@@ -65,7 +82,8 @@ function App () {
       </div>
 
       <p>Tamaño del DT (En TR) : {total}</p>
-      <p>{select}</p>
+      <p>{centrifugo}</p>
+      <p>{absorcion}</p>
 
       <Tables caudal={caudal} temp1={temp1} temp2={temp2} servicio={servicio} total={total} />
     </>
--- src/logic/selectStyles.js
+++ src/logic/selectStyles.js
@@ -1,31 +1,26 @@
-export const selectStyle = () => {
-  const customStyles = {
-    control: (base, state) => ({
-      ...base,
-      border: '1px solid #DADCE0',
-      height: '45px',
-      width: '90%',
-      marginBottom: '17px',
-      background: 'none',
-      padding: '5px 20px',
-      fontSize: '16px',
-      borderRadius: '10px',
-      '&:hover': {
-        borderColor: state.isFocused ? '#4DC3FA' : 'white'
-      }
-    }),
-    menu: base => ({
-      ...base,
-      background: '#1F2739',
-      color: 'white',
-      borderRadius: 0,
-      marginTop: 0
-    }),
-    menuList: base => ({
-      ...base,
-      padding: 0
-    })
-  }
 
-  return customStyles
+export const customStyles = {
+  control: (base, state) => ({
+    ...base,
+    border: '1px solid #DADCE0',
+    height: '55px',
+    width: '120%',
+    background: 'none',
+    fontSize: '16px',
+    borderRadius: '10px',
+    '&:hover': {
+      borderColor: state.isFocused ? '#4DC3FA' : 'white'
+    }
+  }),
+  menu: base => ({
+    ...base,
+    background: '#1F2739',
+    color: 'white',
+    borderRadius: 0,
+    marginTop: 0
+  }),
+  menuList: base => ({
+    ...base,
+    padding: 0
+  })
 }
